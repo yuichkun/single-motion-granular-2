54429b9d9ca4fddbc69cfc130bd0d8364b0eaf30
yuichkun <yuichkun@gmail.com>
2023-02-25 16:11:00 +0900

    load dependency

--- src/rnbo.ts
+++ src/rnbo.ts
@@ -3,5 +3,7 @@ export const setupRnbo = async (context: AudioContext) => {
   const rawPatcher = await fetch("patch.export.json"); // これでhttpリクエストを投げることができる
   const patcher = await rawPatcher.json();
   const device = await createDevice({ patcher, context });
+  const dependencies = await (await fetch("dependencies.json")).json();
+  await device.loadDataBufferDependencies(dependencies); // bufferをロードする
   return device;
 };
