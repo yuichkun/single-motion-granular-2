b5351de1df5aa991bf76c795c094e8cc529faef5
yuichkun <yuichkun@gmail.com>
2023-02-25 15:59:04 +0900

    create audio context

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
+  import Granular from "./Granular.svelte";
+
+  const context = new AudioContext();
+  let isStarted = false;
+
+  const onClick = async () => {
+    await context.resume();
+    isStarted = true;
+  };
 </script>
 
 <main>
-  <h1>single motion granular 2</h1>
-  <button>play</button>
+  {#if isStarted}
+    <Granular {context} />
+  {:else}
+    <h1>single motion granular 2</h1>
+    <button on:click={onClick}>play</button>
+  {/if}
 </main>
 
 <style>
---
+++ src/Granular.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  export let context: AudioContext;
+</script>
+
+<div>グラニュラーだよ</div>
