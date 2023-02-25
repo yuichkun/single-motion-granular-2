50cfd9d67a165b5b4b6db72168b535f92280c235
yuichkun <yuichkun@gmail.com>
2023-02-25 16:30:36 +0900

    create xy pad

--- src/Granular.svelte
+++ src/Granular.svelte
@@ -2,6 +2,7 @@
   import type { Device } from "@rnbo/js";
   import { onMount } from "svelte";
   import { setupMicrophoneSource } from "./stream";
+  import { draggable } from "@neodrag/svelte";
 
   export let context: AudioContext;
   export let device: Device;
@@ -21,5 +22,24 @@
 </script>
 
 <div>グラニュラーだよ</div>
+
+<div id="dragAreaContainer">
+  <div id="dragArea" use:draggable={{ bounds: "parent" }} />
+</div>
 <button on:click={enableGranular}>有効化</button>
 <button on:click={disableGranular}>無効化</button>
+
+<style>
+  #dragAreaContainer {
+    background-color: rgb(30, 30, 30);
+    width: 400px;
+    height: 400px;
+  }
+  #dragArea {
+    background-color: white;
+    width: 25px;
+    height: 25px;
+    border-radius: 38px;
+    cursor: pointer;
+  }
+</style>
